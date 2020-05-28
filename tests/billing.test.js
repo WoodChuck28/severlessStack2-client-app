dcacd14fd185f6ca650e6c26cdca48d90b5751dd
Charles and Monica Daab <Charles.Daab@gmail.com>
2020-05-28 08:32:04 -0400

    First commit

---
+++ tests/billing.test.js
@@ -0,0 +1,37 @@
+
+function calculateCost(storage) {
+    const rate = storage <= 10
+      ? 4
+      : storage <= 100
+        ? 2
+        : 1;
+
+    return rate * storage * 100;
+  }
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
